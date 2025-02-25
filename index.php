5d1f1733ff254a079c0340bb30b8f431906d3b6f
alejandralopeztrigo <alejandra.trigo@eurotransportcar.com>
2025-02-25 11:25:07 +0100

    feat(Frontend): :lipstick: Change field status

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
                         <span><?php echo htmlspecialchars($task['task']); ?></span>
                         <form method="post" style="display:inline;">
                             <button type="submit" name="complete" value="<?php echo htmlspecialchars($task['id']); ?>">
-                                Complete
+                                Completados
                             </button>
                         </form>
                     <?php endif; ?>
